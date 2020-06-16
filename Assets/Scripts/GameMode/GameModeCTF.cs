1ddc01416506cac3f2602a039dee07b4e90713c0
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2020-06-16 20:37:54 +1000

    Update #3

---
+++ Assets/Scripts/GameMode/GameModeCTF.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Capture the flag GameMode
+/// </summary>
+public class GameModeCTF : GameMode
+{
+    List<Flag> flags;
+
+    /*private void Start()
+    {
+        base.Start();
+    }*/
+}
---
+++ Assets/Scripts/GameMode/GameModeFFA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Free for fall GameMode
+/// </summary>
+public class GameModeFFA : GameMode
+{
+    const string typeName = "FFA";
+    GameObject[] gameObjects;
+    private GameObject worldGame;
+    private void Awake()
+    {
+        //gameObjects = worldGame.FindGameObjectsWithTag(typeName);
+    }
+
+    /*private void Start()
+    {
+        base.Start();
+    }*/
+}
